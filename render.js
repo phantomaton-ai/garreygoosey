8d678e22dbf57cf03ad3ee06da8110d0de4d06c6
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-05-16 20:01:56 +0000

    SOURCES

--- render.js
+++ render.js
@@ -21,7 +21,7 @@ ${body}
 const commands = [metamagic(
   'image',
   async ({ file }, prompt) => {
-    const output = path.join(PARK, file);
+    const output = path.join(SOURCES, file);
     if (!fs.existsSync(output)) {
       await phantasia(prompt, { output, width: 960, height: 960 });
     }
@@ -52,13 +52,13 @@ for (const source of sources) {
   const executed = await spellbook.execute(text);
   const parsed = marked.parse(executed);
   const templated = TEMPLATE(parsed);
-  const destination = source.replace(PARK, BUILT).replace(/\.md$/, '.html');
+  const destination = source.replace(SOURCES, BUILT).replace(/\.md$/, '.html');
   fs.writeFileSync(destination, templated, 'utf-8');
 }
 
-const images = glob.globSync(path.join(PARK, '**', '*.png'));
+const images = glob.globSync(path.join(SOURCES, '**', '*.png'));
 
 for (const image of images) {
-  const target = image.replace(PARK, BUILT);
+  const target = image.replace(SOURCES, BUILT);
   fs.copyFileSync(image, target);
 }
