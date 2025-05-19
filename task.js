c4fb2fe4c0b4b53184a8d44694c35aa828570f63
Doc Woe <woe@phantomaton.com>
2025-05-19 14:42:54 -0700

    task

---
+++ task.js
@@ -0,0 +1,6 @@
+const task = ({ topic, panel }) => topic ? panel ?
+  `Draw panel ${panel + 1}` :
+  `Write a script for the topic: ${topic}` :
+  'Come up with a new topic.';
+
+export default commands;
