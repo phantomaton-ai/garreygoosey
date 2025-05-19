5477eb081a6e2defe83b9575219d182438e86c6d
Doc Woe <woe@phantomaton.com>
2025-05-19 14:43:50 -0700

    instruct

--- instruct.js
+++ instruct.js
@@ -1,6 +1,6 @@
-const instruction = ({ topic, panel }) => topic ? panel ?
+const instruct = ({ topic, panel }) => topic ? panel ?
   `Draw panel ${panel + 1}` :
   `Write a script for the topic: ${topic}` :
   'Come up with a new topic.';
 
-export default instruction;
+export default instruct;
