c4c24d13337b0fff0966a0f87d98f2bd41a558cc
Doc Woe <woe@phantomaton.com>
2025-05-22 11:38:34 -0700

    trim

--- subtask.js
+++ subtask.js
@@ -1,5 +1,4 @@
 import author from './author.js';
-import propose from './propose.js';
 import sketch from './sketch.js';
 
 const subtask = configuration => (configuration.topic && configuration.panel)
