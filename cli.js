8281ec866862daf5bb270b9d3c4b2011d6e94643
Doc Woe <woe@phantomaton.com>
2025-05-24 14:45:26 -0700

    home

--- cli.js
+++ cli.js
@@ -4,10 +4,14 @@ const flags = process.argv.slice(2).filter(arg => arg.startsWith('-'));
 const nonflags = process.argv.slice(2).filter(arg => !arg.startsWith('-'));
 
 const debug = flags.includes('--debug');
+
+const prefix = '--home=';
+const home = flags.find(flag => flag.startsWith(prefix))?.slice(prefix.length);
+
 const [ topic, panel ] = nonflags;
 
 try {
-  console.log(await garreygoosey({ debug, topic, panel }));
+  console.log(await garreygoosey({ debug, home, topic, panel }));
 } catch (e) {
   console.log('ERROR', e, JSON.stringify(e,null,2));
 }
